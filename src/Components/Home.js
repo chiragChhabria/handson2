32de489d2f228aefa075423921a4b039b2ae9183
chiku46 <chiragchhabriamaster@gmail.com>
2021-02-10 09:34:08 +0530

    Trying third time

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>Welcome to the Home page of Student Management Portal</h2>
+            </div>
+        );
+    }
+}
+export default Home;
